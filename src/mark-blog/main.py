8821e5343c855604bd358940773028937c82fd77
pchchv <ipchchv@gmail.com>
2024-03-28 13:18:11 +0300

    Refactoring

--- src/mark-blog/main.py
+++ src/mark-blog/main.py
@@ -5,12 +5,12 @@ from router import get_router
 
 
 def get_blog_app(
-        app: FastAPI,
-        prefix: str | None = "blog",
+    app: FastAPI,
+    prefix: str | None = "blog",
 ) -> FastAPI:
+    # Router controls
     router = get_router()
     router_kwargs: dict[str, Any] = {"router": router, "tags": ["blog"]}
-
     if prefix is not None:
         router_kwargs["prefix"] = f"/{prefix}"
     app.include_router(**router_kwargs)
