058fae8b8c75531ca498e726e72b5280789f3699
pchchv <ipchchv@gmail.com>
2024-03-28 12:59:55 +0300

    Implement a function that creates a blog application

--- src/mark-blog/main.py
+++ src/mark-blog/main.py
@@ -1,3 +1,20 @@
+from typing import Any
 from fastapi import FastAPI
+from router import get_router
 
-app = FastAPI()
+
+app = blog_app(FastAPI())
+
+
+def blog_app(
+        app: FastAPI,
+        prefix: str | None = "blog",
+) -> FastAPI:
+    router = get_router()
+    router_kwargs: dict[str, Any] = {"router": router, "tags": ["blog"]}
+
+    if prefix is not None:
+        router_kwargs["prefix"] = f"/{prefix}"
+    app.include_router(**router_kwargs)
+
+    return app
