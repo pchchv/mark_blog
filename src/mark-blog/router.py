1351f6f862762871b4a35307545b976f81b0584a
pchchv <ipchchv@gmail.com>
2024-03-29 12:31:44 +0300

    Implement a blog_index func. Get route

--- src/mark-blog/router.py
+++ src/mark-blog/router.py
@@ -1,6 +1,10 @@
-from fastapi import APIRouter
+from typing import Any
+
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+import helpers
 
 def get_router(
         templates: Jinja2Templates,
@@ -9,7 +13,18 @@ def get_router(
     router = APIRouter()
     
     @router.get("/")
-    def get():
-        return "Blog app"
+    async def blog_index(request: Request, response_class=HTMLResponse):
+        posts = helpers.list_posts()
+        recent_3 = posts[:3]
+
+        favorite_posts: list[dict[Any, Any]] = list(
+            filter(lambda x: x["slug"] in favorite_post_ids, posts)
+        )
+
+        return templates.TemplateResponse(
+            request=request,
+            name="index.html",
+            context={"recent_3": recent_3, "favorite_posts": favorite_posts},
+        )
     
     return router
