cdac7e88af50203d8acfa3c61b23acab097b7806
pchchv <ipchchv@gmail.com>
2024-03-30 13:51:13 +0300

    Refactoring

--- src/mark-blog/router.py
+++ src/mark-blog/router.py
@@ -1,17 +1,17 @@
 from typing import Any
-
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 import helpers
 
+
 def get_router(
-        templates: Jinja2Templates,
-        favorite_post_ids: set[str] = set()
+    templates: Jinja2Templates,
+    favorite_post_ids: set[str] = set()
 ) -> APIRouter:
     router = APIRouter()
-    
+
     @router.get("/")
     async def blog_index(request: Request, response_class=HTMLResponse):
         posts = helpers.list_posts()
@@ -26,5 +26,5 @@ def get_router(
             name="index.html",
             context={"recent_3": recent_3, "favorite_posts": favorite_posts},
         )
-    
+
     return router
