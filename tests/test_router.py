6538b1bc531f9ae0e20c1cd4a504754e03b2ec34
pchchv <ipchchv@gmail.com>
2024-03-30 14:17:28 +0300

    Implement a client test

--- tests/test_router.py
+++ tests/test_router.py
@@ -1,9 +1,12 @@
 import jinja2
 import mark_blog
 from fastapi import FastAPI
+from fastapi.testclient import TestClient
 
 
 app = FastAPI()
 app = mark_blog.get_blog_app(
     app, jinja2_loader=jinja2.FileSystemLoader("src/mark_blog/templates")
 )
+
+client = TestClient(app)
