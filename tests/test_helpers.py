5d1ba82eec0c31a35215ad793a1a14152ffc05dc
pchchv <ipchchv@gmail.com>
2024-03-30 14:05:02 +0300

    Implement a list_published_posts_failure test

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -6,3 +6,8 @@ def test_list_published_posts_success():
     assert len(posts) == 19
     assert posts[0]["title"] == "No Tags"
     assert posts[-1]["title"] == "Code, Code, Code"
+
+
+def test_list_published_posts_failure():
+    posts = helpers.list_posts(posts_dirname="blarg")
+    assert len(posts) == 0
